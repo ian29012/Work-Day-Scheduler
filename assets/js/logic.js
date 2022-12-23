69a521fc8154f98a3a0fe9f1cc270dd4a27165db
ian29012 <iancheng29012@gmail.com>
2022-12-23 17:13:46 +0000

    updated the localstorage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,9 @@
 var dayDisplay = $("#currentDay") ;
 var timeDisplay = $ ("#currentTime") ;
+const timeID = document.querySelectorAll("#time");
+var saveBtn = $(".saveBtn");
+var rowID = $(".row"); 
+var timeLine = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Current time display 
 function currentTime(){
@@ -14,8 +18,6 @@ setInterval(currentTime, 1000);
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 function whatTime(){
     var rightNow = String(moment().format("H"));
-    var timeLine = [8, 9, 11, 12, 13, 14, 15, 16, 17];
-    const timeID = document.querySelectorAll("#time")
 
     for( var i = 0; i < timeLine.length; i++ ){
         if ( rightNow < timeLine[i] ){
@@ -25,7 +27,24 @@ function whatTime(){
         } else {
            $(timeID[i]).addClass("past")
         }
-    }
+    };
 };
+// Save the event in local storage when the save button is clicked in that timeblock.
+function saveEvent(event){
+    event.preventDefault();
+    var valInput = $(this).siblings("textarea").val();
+    var valId = this.parentElement.id;
+    localStorage.setItem(valId, valInput);
+    init();
+}
+
+function init(){
+    for ( var i = 0; i < timeLine.length; i++ ){
+    $(timeID[i]).text(localStorage.getItem(timeLine[i]));
+    console.log(localStorage.getItem(timeLine[i]))
+}};
+
+saveBtn.on("click", saveEvent)
 
 whatTime();
+init();
